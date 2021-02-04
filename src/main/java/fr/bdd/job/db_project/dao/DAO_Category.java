fb71b3f202d23860a8921bea5300596a86f2f664
Norphiil <norphiil@hotmail.fr>
2021-02-04 17:24:49 +0100

    Add DAO_Category and DAO_Condition

---
+++ src/main/java/fr/bdd/job/db_project/dao/DAO_Category.java
@@ -0,0 +1,383 @@
+package fr.bdd.job.db_project.dao;
+
+import fr.bdd.custom.sql.PreparedStatementAware;
+import fr.bdd.dataconnection.DataConnection;
+import fr.bdd.job.dao.Dao;
+import fr.bdd.job.db_project.jobclass.Category;
+import fr.bdd.log.generate.CustomLogger;
+
+import java.security.InvalidKeyException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * DAO pattern class.
+ * Used for the job class {@link Category}
+ *
+ * @author Gaetan Brenckle
+ */
+public class DAO_Category implements Dao<Category> {
+
+    private static final CustomLogger LOGGER = CustomLogger.create(DAO_Category.class.getName());
+    private DataConnection connectionHandle_ = null;
+
+    /**
+     * Default constructor.
+     * Need to call {@link DAO_Category#setConnection(DataConnection)} before any other function.
+     *
+     * @author Gaetan Brenckle
+     */
+    public DAO_Category() {
+    }
+
+    /**
+     * Constructor with the Connection argument.
+     *
+     * @author Gaetan Brenckle
+     *
+     * @param conn - {@link DataConnection} - connection used.
+     */
+    public DAO_Category(DataConnection conn) throws InvalidKeyException {
+        this.connectionHandle_ = conn;
+    }
+
+    /**
+     * Setter of the connection.
+     *
+     * @author Gaetan Brenckle
+     *
+     * @param conn - {@link DataConnection} - Connection used.
+     */
+    @Override
+    public void setConnection(DataConnection conn) throws InvalidKeyException {
+        this.connectionHandle_ = conn;
+    }
+
+
+
+    /**
+     * SELECT with the index of the associate job class.
+     *
+     * @author Gaetan Brenckle
+     *
+     * @param id - {@link String} - index of the associate job class. Can handle null.
+     * @return - {@link Category} - the job class that can be found with the index
+     * @throws SQLException - throw the exception to force a try catch when used.
+     * @throws InvalidKeyException - throw this exception when the given list dont have the key wanted
+     */
+    public final Category select(String id) throws SQLException, InvalidKeyException {
+        Category retCategory = null;
+
+        if (id == null) {
+            if (LOGGER.isWarnEnabled()) {
+                LOGGER.warn("Category select when the given id is null");
+            }
+            return retCategory;
+        }
+
+        final String select_sql = String.format("%s %s %s",
+                String.format("SELECT %s, %s, %s", "category_ID", "dataCategory", "prefix"),
+                String.format("FROM %s ", "Category"),
+                String.format("WHERE id = ?"));
+
+        final PreparedStatementAware prepSelect = new PreparedStatementAware(connectionHandle_.getConnection().prepareStatement(select_sql));
+        prepSelect.setString(id);
+
+        try(final ResultSet resultSelect = prepSelect.executeQuery()) {
+            if (resultSelect.next()) {
+                retCategory =
+                        new Category()
+                                .setcategory_ID(resultSelect.getString("category_ID"))
+                                .setdataCategory(resultSelect.getString("dataCategory"))
+                                .setprefix(resultSelect.getString("prefix"));
+
+            }
+        }
+        return retCategory;
+    }
+
+    /**
+     * SELECT with the index of the associate job class.
+     *
+     * @author Gaetan Brenckle
+     *
+     * @param map - {@link HashMap<String, String>} - index of the associate job class. Can handle null.
+     * @return - {@link Category} - the job class that can be found with the index
+     * @throws SQLException - throw the exception to force a try catch when used.
+     * @throws InvalidKeyException - throw this exception when the given list dont have the key wanted
+     */
+    public final List<Category> selectByMultiCategory(HashMap<String, String> map) throws SQLException, InvalidKeyException {
+        final List<Category> retCategories = new ArrayList<>();
+
+        if (map == null) {
+            if (LOGGER.isWarnEnabled()) {
+                LOGGER.warn("Category select when the given id is null");
+            }
+            return retCategories;
+        }
+
+
+        StringBuilder select_sql = new StringBuilder(String.format("%s %s %s",
+                String.format("SELECT %s, %s, %s", "category_ID", "dataCategory", "prefix"),
+                String.format("FROM %s ", "Category"),
+                String.format("WHERE 1=1")
+        ));
+
+        for (Map.Entry<String,String> entry : map.entrySet()) {
+            select_sql.append(String.format(" AND %s = ?", entry.getKey()));
+        }
+
+
+        final PreparedStatementAware prepSelect = new PreparedStatementAware(connectionHandle_.getConnection().prepareStatement(select_sql.toString()));
+        for (Map.Entry<String,String> entry : map.entrySet()) {
+            prepSelect.setString(entry.getValue());
+        }
+
+        try(final ResultSet resultSelect = prepSelect.executeQuery()) {
+            if (resultSelect.next()) {
+                Category Category =
+                        new Category().setcategory_ID(resultSelect.getString("category_ID"))
+                                    .setdataCategory(resultSelect.getString("dataCategory"))
+                                    .setprefix(resultSelect.getString("prefix"));
+            }
+        }
+        return retCategories;
+    }
+
+    /**
+     * SELECT of all occurance of the Category class.
+     *
+     * @author Gaetan Brenckle
+     *
+     * @return - {@link List} - a list that contain all occurance of {@link Category}, the job class associate.
+     * @throws SQLException - throw the exception to force a try catch when used.
+     * @throws InvalidKeyException - throw this exception when the given list dont have the key wanted
+     */
+    @Override
+    public List<Category> selectAll() throws SQLException, InvalidKeyException {
+        final List<Category> retCategories = new ArrayList<>();
+
+        String format = String.format("%s %s",
+                String.format("SELECT %s, %s, %s", "category_ID", "dataCategory", "prefix"),
+                String.format("FROM %s ", "Category"));
+        final String selectAll_sql = format;
+
+        final PreparedStatementAware prepSelectAll = new PreparedStatementAware(connectionHandle_.getConnection().prepareStatement(selectAll_sql));
+
+
+        try(final ResultSet resultSelectAll = prepSelectAll.executeQuery()) {
+            if (resultSelectAll.next()) {
+                Category Category =
+                        new Category()
+                                .setcategory_ID(resultSelectAll.getString("category_ID"))
+                                .setdataCategory(resultSelectAll.getString("dataCategory"))
+                                .setprefix(resultSelectAll.getString("prefix"));
+                retCategories.add(Category);
+            }
+        }
+
+        return retCategories;
+    }
+
+    @Override
+    public List<Category> selectAll(List<Category> excludeList) throws SQLException, InvalidKeyException {
+        return null;
+    }
+
+    /**
+     * INSERT the job class.
+     * Cannot use (ODBC table)
+     *
+     * @author Gaetan Brenckle
+     *
+     * @param obj - {@link Category} - insert the job class.
+     * @return - boolean - the state of the sql insert.
+     * @throws SQLException - throw the exception to force a try catch when used.
+     * @throws InvalidKeyException - throw this exception when the given list dont have the key wanted
+     */
+    @Override
+    public boolean insert(Category obj) throws SQLException, InvalidKeyException {
+        boolean retBool = true;
+
+        if (obj == null) {
+            if (LOGGER.isWarnEnabled()) {
+                LOGGER.warn(String.format("%s on parameter to insert is null.", "Category"));
+            }
+            return false;
+        }
+
+        if (obj.getcategory_ID() == null || obj.getcategory_ID().isEmpty()) {
+            if (LOGGER.isWarnEnabled()) {
+                LOGGER.warn(String.format("The parameter %s on the class is null", "category_ID"));
+            }
+            retBool = false;
+        }
+
+        if (obj.getdataCategory() == null || obj.getdataCategory().isEmpty()) {
+            if (LOGGER.isWarnEnabled()) {
+                LOGGER.warn(String.format("The parameter %s on the class is null", "dataCategory"));
+            }
+            retBool = false;
+        }
+
+        if (obj.getprefix() == null || obj.getprefix().isEmpty()) {
+            if (LOGGER.isWarnEnabled()) {
+                LOGGER.warn(String.format("The parameter %s on the class is null", "prefix"));
+            }
+            retBool = false;
+        }
+
+        if (!retBool) {
+            return false;
+        }
+
+        String insert_sql = String.format("%s %s ",
+                String.format("INSERT INTO %s (%s, %s, %s)", "Category", "category_ID", "dataCategory", "prefix"),
+                "VALUES (?, ?, ?)");
+
+        final PreparedStatementAware prepInsert = new PreparedStatementAware(connectionHandle_.getConnection().prepareStatement(insert_sql));
+        prepInsert.setString(obj.getcategory_ID());
+        prepInsert.setString(obj.getdataCategory());
+        prepInsert.setString(obj.getprefix());
+
+        retBool = prepInsert.executeUpdate() > 0;
+
+        if (LOGGER.isInfoEnabled() && retBool) {
+            String printedSql = String.format("Insert a new %s : [%s]", "Category", prepInsert.printSqlStatement(insert_sql));
+
+            LOGGER.info(printedSql);
+            // DbLogger.getInstance().dbLog(Level.INFO, printedSql);
+        }
+        return retBool;
+    }
+
+    /**
+     * UPDATE the job class.
+     * Cannot use (ODBC table)
+     *
+     * @author Gaetan Brenckle
+     *
+     * @param obj - {@link Category} - insert the job class.
+     * @return - boolean - the state of the sql update.
+     * @throws SQLException - throw the exception to force a try catch when used.
+     * @throws InvalidKeyException - throw this exception when the given list dont have the key wanted
+     */
+    @Override
+    public boolean update(Category obj) throws SQLException, InvalidKeyException {
+        boolean retBool = true;
+
+        if (obj == null) {
+            if (LOGGER.isWarnEnabled()) {
+                LOGGER.warn(String.format("%s on parameter to insert is null.", "Category"));
+            }
+            return false;
+        }
+
+        if (obj.getcategory_ID() == null || obj.getcategory_ID().isEmpty()) {
+            if (LOGGER.isWarnEnabled()) {
+                LOGGER.warn(String.format("The parameter %s on the class is null", "category_ID"));
+            }
+            retBool = false;
+        }
+
+        if (obj.getdataCategory() == null || obj.getdataCategory().isEmpty()) {
+            if (LOGGER.isWarnEnabled()) {
+                LOGGER.warn(String.format("The parameter %s on the class is null", "dataCategory"));
+            }
+            retBool = false;
+        }
+
+        if (obj.getprefix() == null || obj.getprefix().isEmpty()) {
+            if (LOGGER.isWarnEnabled()) {
+                LOGGER.warn(String.format("The parameter %s on the class is null", "prefix"));
+            }
+            retBool = false;
+        }
+
+        if (!retBool) {
+            return false;
+        }
+
+        String update_sql = String.format("%s %s %s %s %s ",
+                String.format("UPDATE %s", "Category"),
+                String.format("SET %s = ?", "category_ID"),
+                String.format("SET %s = ?", "dataCategory"),
+                String.format("SET %s = ?", "prefix"),
+                String.format("WHERE %s = ?", "Category_ID"));
+
+        final PreparedStatementAware prepUpdate = new PreparedStatementAware(connectionHandle_.getConnection().prepareStatement(update_sql));
+        prepUpdate.setString(obj.getcategory_ID());
+        prepUpdate.setString(obj.getdataCategory());
+        prepUpdate.setString(obj.getprefix());
+
+        retBool = prepUpdate.executeUpdate() > 0;
+
+        if (LOGGER.isInfoEnabled() && retBool) {
+            String printedSql = String.format("Update a new %s : [%s]", "Category", prepUpdate.printSqlStatement(update_sql));
+
+            LOGGER.info(printedSql);
+            // DbLogger.getInstance().dbLog(Level.INFO, printedSql);
+        }
+        return retBool;
+    }
+
+    @Override
+    public boolean upsert(Category obj) throws SQLException, InvalidKeyException {
+        return false;
+    }
+
+    /**
+     * DELETE the job class.
+     * Cannot use (ODBC table)
+     *
+     * @author Gaetan Brenckle
+     *
+     * @param obj - {@link Category} - insert the job class.
+     * @return - boolean - the state of the sql delete.
+     * @throws SQLException - throw the exception to force a try catch when used.
+     */
+    @Override
+    public boolean delete(Category obj) throws SQLException {
+        boolean retBool = true;
+
+        if (obj == null) {
+            if (LOGGER.isWarnEnabled()) {
+                LOGGER.warn(String.format("%s on parameter to insert is null.", "Category"));
+            }
+            return false;
+        }
+
+        if (obj.getcategory_ID() == null || obj.getcategory_ID().isEmpty()) {
+            if (LOGGER.isWarnEnabled()) {
+                LOGGER.warn(String.format("The parameter %s on the class is null", "category_ID"));
+            }
+            retBool = false;
+        }
+
+        if (!retBool) {
+            return false;
+        }
+
+        String delete_sql = String.format("%s %s ",
+                String.format("DELETE FROM %s", "Category"),
+                String.format("WHERE %s = ?", "Category_ID"));
+
+        final PreparedStatementAware prepDelete = new PreparedStatementAware(connectionHandle_.getConnection().prepareStatement(delete_sql));
+        prepDelete.setString(obj.getcategory_ID());
+
+        retBool = prepDelete.executeUpdate() > 0;
+
+        if (LOGGER.isInfoEnabled() && retBool) {
+            String printedSql = String.format("Delete a new %s : [%s]", "Category", prepDelete.printSqlStatement(delete_sql));
+
+            LOGGER.info(printedSql);
+            // DbLogger.getInstance().dbLog(Level.INFO, printedSql);
+        }
+
+        return retBool;
+    }
+}
---
+++ src/main/java/fr/bdd/job/db_project/dao/DAO_Condition.java
@@ -0,0 +1,345 @@
+package fr.bdd.job.db_project.dao;
+
+import fr.bdd.custom.sql.PreparedStatementAware;
+import fr.bdd.dataconnection.DataConnection;
+import fr.bdd.job.dao.Dao;
+import fr.bdd.job.db_project.jobclass.Condition;
+import fr.bdd.log.generate.CustomLogger;
+import org.apache.logging.log4j.Level;
+
+import java.security.InvalidKeyException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * DAO pattern class.
+ * Used for the job class {@link Condition}
+ *
+ * @author Gaetan Brenckle
+ */
+public class DAO_Condition implements Dao<Condition> {
+
+    private static final CustomLogger LOGGER = CustomLogger.create(DAO_Condition.class.getName());
+    private DataConnection connectionHandle_ = null;
+
+    /**
+     * Default constructor.
+     * Need to call {@link DAO_Condition#setConnection(DataConnection)} before any other function.
+     *
+     * @author Gaetan Brenckle
+     */
+    public DAO_Condition() {
+    }
+
+    /**
+     * Constructor with the Connection argument.
+     *
+     * @author Gaetan Brenckle
+     *
+     * @param conn - {@link DataConnection} - connection used.
+     */
+    public DAO_Condition(DataConnection conn) throws InvalidKeyException {
+        this.connectionHandle_ = conn;
+    }
+
+    /**
+     * Setter of the connection.
+     *
+     * @author Gaetan Brenckle
+     *
+     * @param conn - {@link DataConnection} - Connection used.
+     */
+    @Override
+    public void setConnection(DataConnection conn) throws InvalidKeyException {
+        this.connectionHandle_ = conn;
+    }
+
+
+
+    /**
+     * SELECT with the index of the associate job class.
+     *
+     * @author Gaetan Brenckle
+     *
+     * @param id - {@link String} - index of the associate job class. Can handle null.
+     * @return - {@link Condition} - the job class that can be found with the index
+     * @throws SQLException - throw the exception to force a try catch when used.
+     * @throws InvalidKeyException - throw this exception when the given list dont have the key wanted
+     */
+    public final Condition select(String id) throws SQLException, InvalidKeyException {
+        Condition retCondition = null;
+
+        if (id == null) {
+            if (LOGGER.isWarnEnabled()) {
+                LOGGER.warn("Condition select when the given id is null");
+            }
+            return retCondition;
+        }
+
+        final String select_sql = String.format("%s %s %s",
+                String.format("SELECT %s", "condition_ID"),
+                String.format("FROM %s ", "Condition"),
+                String.format("WHERE id = ?"));
+
+        final PreparedStatementAware prepSelect = new PreparedStatementAware(connectionHandle_.getConnection().prepareStatement(select_sql));
+        prepSelect.setString(id);
+
+        try(final ResultSet resultSelect = prepSelect.executeQuery()) {
+            if (resultSelect.next()) {
+                retCondition =
+                        new Condition()
+                                .setcondition_ID(resultSelect.getString("condition_ID"));
+
+            }
+        }
+        return retCondition;
+    }
+
+    /**
+     * SELECT with the index of the associate job class.
+     *
+     * @author Gaetan Brenckle
+     *
+     * @param map - {@link HashMap<String, String>} - index of the associate job class. Can handle null.
+     * @return - {@link Condition} - the job class that can be found with the index
+     * @throws SQLException - throw the exception to force a try catch when used.
+     * @throws InvalidKeyException - throw this exception when the given list dont have the key wanted
+     */
+    public final List<Condition> selectByMultiCondition(HashMap<String, String> map) throws SQLException, InvalidKeyException {
+        final List<Condition> retConditions = new ArrayList<>();
+
+        if (map == null) {
+            if (LOGGER.isWarnEnabled()) {
+                LOGGER.warn("Condition select when the given id is null");
+            }
+            return retConditions;
+        }
+
+
+        StringBuilder select_sql = new StringBuilder(String.format("%s %s %s",
+                String.format("SELECT %s", "condition_ID"),
+                String.format("FROM %s ", "Condition"),
+                String.format("WHERE 1=1")
+        ));
+
+        for (Map.Entry<String,String> entry : map.entrySet()) {
+            select_sql.append(String.format(" AND %s = ?", entry.getKey()));
+        }
+
+
+        final PreparedStatementAware prepSelect = new PreparedStatementAware(connectionHandle_.getConnection().prepareStatement(select_sql.toString()));
+        for (Map.Entry<String,String> entry : map.entrySet()) {
+            prepSelect.setString(entry.getValue());
+        }
+
+        try(final ResultSet resultSelect = prepSelect.executeQuery()) {
+            if (resultSelect.next()) {
+                Condition condition =
+                        new Condition().setcondition_ID(resultSelect.getString("condition_ID"));
+            }
+        }
+        return retConditions;
+    }
+
+    /**
+     * SELECT of all occurance of the Condition class.
+     *
+     * @author Gaetan Brenckle
+     *
+     * @return - {@link List} - a list that contain all occurance of {@link Condition}, the job class associate.
+     * @throws SQLException - throw the exception to force a try catch when used.
+     * @throws InvalidKeyException - throw this exception when the given list dont have the key wanted
+     */
+    @Override
+    public List<Condition> selectAll() throws SQLException, InvalidKeyException {
+        final List<Condition> retConditions = new ArrayList<>();
+
+        String format = String.format("%s %s %s",
+                String.format("SELECT %s", "condition_ID"),
+                String.format("FROM %s ", "Condition"));
+        final String selectAll_sql = format;
+
+        final PreparedStatementAware prepSelectAll = new PreparedStatementAware(connectionHandle_.getConnection().prepareStatement(selectAll_sql));
+
+
+        try(final ResultSet resultSelectAll = prepSelectAll.executeQuery()) {
+            if (resultSelectAll.next()) {
+                Condition condition =
+                        new Condition()
+                                .setcondition_ID(resultSelectAll.getString("condition_ID"));
+                retConditions.add(condition);
+            }
+        }
+
+        return retConditions;
+    }
+
+    @Override
+    public List<Condition> selectAll(List<Condition> excludeList) throws SQLException, InvalidKeyException {
+        return null;
+    }
+
+    /**
+     * INSERT the job class.
+     * Cannot use (ODBC table)
+     *
+     * @author Gaetan Brenckle
+     *
+     * @param obj - {@link Condition} - insert the job class.
+     * @return - boolean - the state of the sql insert.
+     * @throws SQLException - throw the exception to force a try catch when used.
+     * @throws InvalidKeyException - throw this exception when the given list dont have the key wanted
+     */
+    @Override
+    public boolean insert(Condition obj) throws SQLException, InvalidKeyException {
+        boolean retBool = true;
+
+        if (obj == null) {
+            if (LOGGER.isWarnEnabled()) {
+                LOGGER.warn(String.format("%s on parameter to insert is null.", "Condition"));
+            }
+            return false;
+        }
+
+        if (obj.getcondition_ID() == null || obj.getcondition_ID().isEmpty()) {
+            if (LOGGER.isWarnEnabled()) {
+                LOGGER.warn(String.format("The parameter %s on the class is null", "condition_ID"));
+            }
+            retBool = false;
+        }
+
+        if (!retBool) {
+            return false;
+        }
+
+        String insert_sql = String.format("%s %s ",
+                String.format("INSERT INTO %s (%s)", "Condition", "condition_ID"),
+                "VALUES (?)");
+
+        final PreparedStatementAware prepInsert = new PreparedStatementAware(connectionHandle_.getConnection().prepareStatement(insert_sql));
+        prepInsert.setString(obj.getcondition_ID());
+
+        retBool = prepInsert.executeUpdate() > 0;
+
+        if (LOGGER.isInfoEnabled() && retBool) {
+            String printedSql = String.format("Insert a new %s : [%s]", "Condition", prepInsert.printSqlStatement(insert_sql));
+
+            LOGGER.info(printedSql);
+            // DbLogger.getInstance().dbLog(Level.INFO, printedSql);
+        }
+        return retBool;
+    }
+
+    /**
+     * UPDATE the job class.
+     * Cannot use (ODBC table)
+     *
+     * @author Gaetan Brenckle
+     *
+     * @param obj - {@link Condition} - insert the job class.
+     * @return - boolean - the state of the sql update.
+     * @throws SQLException - throw the exception to force a try catch when used.
+     * @throws InvalidKeyException - throw this exception when the given list dont have the key wanted
+     */
+    @Override
+    public boolean update(Condition obj) throws SQLException, InvalidKeyException {
+        boolean retBool = true;
+
+        if (obj == null) {
+            if (LOGGER.isWarnEnabled()) {
+                LOGGER.warn(String.format("%s on parameter to insert is null.", "Condition"));
+            }
+            return false;
+        }
+
+        if (obj.getcondition_ID() == null || obj.getcondition_ID().isEmpty()) {
+            if (LOGGER.isWarnEnabled()) {
+                LOGGER.warn(String.format("The parameter %s on the class is null", "condition_ID"));
+            }
+            retBool = false;
+        }
+
+        if (!retBool) {
+            return false;
+        }
+
+        String update_sql = String.format("%s %s %s ",
+                String.format("UPDATE %s", "Condition"),
+                String.format("SET %s = ?", "condition_ID"),
+                String.format("WHERE %s = ?", "condition_ID"));
+
+        final PreparedStatementAware prepUpdate = new PreparedStatementAware(connectionHandle_.getConnection().prepareStatement(update_sql));
+        prepUpdate.setString(obj.getcondition_ID());
+
+        retBool = prepUpdate.executeUpdate() > 0;
+
+        if (LOGGER.isInfoEnabled() && retBool) {
+            String printedSql = String.format("Update a new %s : [%s]", "Condition", prepUpdate.printSqlStatement(update_sql));
+
+            LOGGER.info(printedSql);
+            // DbLogger.getInstance().dbLog(Level.INFO, printedSql);
+        }
+        return retBool;
+    }
+
+    @Override
+    public boolean upsert(Condition obj) throws SQLException, InvalidKeyException {
+        return false;
+    }
+
+    /**
+     * DELETE the job class.
+     * Cannot use (ODBC table)
+     *
+     * @author Gaetan Brenckle
+     *
+     * @param obj - {@link Condition} - insert the job class.
+     * @return - boolean - the state of the sql delete.
+     * @throws SQLException - throw the exception to force a try catch when used.
+     */
+    @Override
+    public boolean delete(Condition obj) throws SQLException {
+        boolean retBool = true;
+
+        if (obj == null) {
+            if (LOGGER.isWarnEnabled()) {
+                LOGGER.warn(String.format("%s on parameter to insert is null.", "Condition"));
+            }
+            return false;
+        }
+
+        if (obj.getcondition_ID() == null || obj.getcondition_ID().isEmpty()) {
+            if (LOGGER.isWarnEnabled()) {
+                LOGGER.warn(String.format("The parameter %s on the class is null", "condition_ID"));
+            }
+            return false;
+        }
+
+        if (!retBool) {
+            return false;
+        }
+
+        String delete_sql = String.format("%s %s ",
+                String.format("DELETE FROM %s", "Condition"),
+                String.format("WHERE %s = ?", "condition_ID"));
+
+        final PreparedStatementAware prepDelete = new PreparedStatementAware(connectionHandle_.getConnection().prepareStatement(delete_sql));
+        prepDelete.setString(obj.getcondition_ID());
+
+        retBool = prepDelete.executeUpdate() > 0;
+
+        if (LOGGER.isInfoEnabled() && retBool) {
+            String printedSql = String.format("Delete a new %s : [%s]", "Condition", prepDelete.printSqlStatement(delete_sql));
+
+            LOGGER.info(printedSql);
+            // DbLogger.getInstance().dbLog(Level.INFO, printedSql);
+        }
+
+        return retBool;
+    }
+}
